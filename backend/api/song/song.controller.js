5893fe45cfbadca5d45a809584816467d3dab35c
barzaken <bar.zaken@boyar.org.il>
2023-01-06 12:25:04 +0200

    added responsive

--- backend/api/song/song.controller.js
+++ backend/api/song/song.controller.js
@@ -1,11 +1,12 @@
 const songService = require('./song.service.js')
+const Youtube = require('youtube-stream-url');
 
 async function getSongById(req,res){
     try{
         let song = await Youtube.getInfo({url: `https://www.youtube.com/watch?v=${req.params.id}`})
         const regex = new RegExp('audio/webm','i');
         song.formats = song.formats.filter(format => regex.test(format.mimeType))
-        res.json(song)
+        res.json(song.formats[0].url)
     }catch(err){
         console.log(err);
     }
--- backend/api/song/song.service.js
+++ backend/api/song/song.service.js
@@ -5,12 +5,15 @@ const Youtube = require('youtube-stream-url');
 async function query(term) {
     try {
         const criteria = {
-            // song_title: { $regex: term, $options: 'i' },
-            // song_artist: { $regex: term, $options: 'i' }
+            song_title: { $regex: term, $options: 'i' },
+            song_artist: { $regex: term, $options: 'i' }
         }
         const collection = await dbService.getCollection('songs')
         var songs = await collection.find(criteria).toArray()
-        if (!songs || !songs.length) songs = await fetchSongs(term)
+        if (!songs || !songs.length){
+            songs = await fetchSongs(term)
+        } 
+        // console.log('before front',songs);
         return songs
     } catch (err) {
         logger.error('cannot find songs', err)
@@ -24,10 +27,33 @@ async function fetchSongs(term) {
         const YOUTUBE_API_URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=AIzaSyCnH_-XZFUZfu4ATQaUyRrKuog3inohJME&q=${term}`;
         var songs = await fetch(YOUTUBE_API_URL).then(res => res.json()).then(data => data.items)
         if(!songs || !songs.length) return null
-        songs = await getSongs(songs)
+        songs = await Promise.all(songs.map(async (song) => {
+            let songToSave = await saveSong(song)
+            return songToSave
+        }
+        ))
+        console.log('after save ',songs);
+        // songs = await getSongs(songs)
         return songs
     }catch(err){
         logger.error('cannot fetch songs',err)
+    }
+}
+
+async function saveSong(song){
+    // {URL,ID,ARTIST,TITLE,COVER,LENGTH}
+    try{
+        let songToSave = {
+            songId: song.id.videoId,
+            song_title:song.snippet.title,
+            song_cover:song.snippet.thumbnails.medium.url,
+            publishedAt:song.snippet.publishedAt,
+            song_artist:song.snippet.channelTitle
+        } 
+        songToSave = await add(songToSave)
+        return songToSave
+    }catch(err){
+        logger.error('Cannot save song',err)
     }
 }
 
@@ -35,8 +61,9 @@ async function fetchSongs(term) {
 async function add(song) {
     try {
         const collection = await dbService.getCollection('songs')
-        const songToSave = await collection.findOne({ id: song.id })
+        const songToSave = await collection.findOne({ songId: song.songId })
         if(!songToSave){
+            console.log('saving');
             await collection.insertOne(song)
         } 
         return song
@@ -46,29 +73,29 @@ async function add(song) {
     }
 }
 
-async function getSongs(songs) {
-    try {
-        songs = songs.map(async (song) => {
-            if(!song.id) return
-            let songData = await Youtube.getInfo({ url: `https://www.youtube.com/watch?v=${song.id.videoId}` })
-            const regex = new RegExp('audio/webm', 'i');
-            songData.formats = songData.formats.filter(format => regex.test(format.mimeType))
-            let songToSave = {
-                url: songData.formats[0].url,
-                id: songData.videoDetails.videoId,
-                song_artist: songData.videoDetails.author,
-                song_cover: songData.videoDetails.thumbnail.thumbnails[0].url,
-                song_title: songData.videoDetails.title
-            }
-             await add(songToSave)
-             return songToSave
-        })
+// async function getSongs(songs) {
+//     try {
+//         songs = songs.map(async (song) => {
+//             if(!song.id) return
+//             let songData = await Youtube.getInfo({ url: `https://www.youtube.com/watch?v=${song.id.videoId}` })
+//             const regex = new RegExp('audio/webm', 'i');
+//             songData.formats = songData.formats.filter(format => regex.test(format.mimeType))
+//             let songToSave = {
+//                 url: songData.formats[0].url,
+//                 id: songData.videoDetails.videoId,
+//                 song_artist: songData.videoDetails.author,
+//                 song_cover: songData.videoDetails.thumbnail.thumbnails[0].url,
+//                 song_title: songData.videoDetails.title
+//             }
+//              await add(songToSave)
+//              return songToSave
+//         })
 
-       return songs
-    } catch (err) {
-        console.log(err);
-    }
-}
+//        return songs
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
 
 module.exports = {
     query,
